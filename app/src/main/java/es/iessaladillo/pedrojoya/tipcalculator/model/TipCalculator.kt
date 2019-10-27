2c264d78a32dc2e8700a81d8c366199dc8b4431e
manuelgonzalezcote <chitaua@gmail.com>
2019-10-27 22:40:22 +0100

    Without TipCalculatorTests

--- app/src/main/java/es/iessaladillo/pedrojoya/tipcalculator/model/TipCalculator.kt
+++ app/src/main/java/es/iessaladillo/pedrojoya/tipcalculator/model/TipCalculator.kt
@@ -3,22 +3,33 @@ package es.iessaladillo.pedrojoya.tipcalculator.model
 
 // TipCalculator class. Its constructor receives bill, percentage and diners
 
-class TipCalculator {
+class TipCalculator(bill: Float, percentage: Float, diners: Int){
+
+    val bill: Float
+    val percentage: Float
+    val diners: Int
+
+    init {
+        this.bill = bill
+        this.percentage = percentage
+        this.diners = diners
+    }
+
 
     fun calculateTip(): Float {
-        // TODO
+        return (bill*percentage)/100
     }
 
     fun calculateTotal(): Float {
-        // TODO
+        return bill + calculateTip()
     }
 
     fun calculatePerDiner(): Float {
-        // TODO
+        return calculateTotal() / diners
     }
 
     fun calculatePerDinerRounded(): Float {
-        // TODO
+        return calculatePerDiner()
     }
 
 }
--- app/src/main/java/es/iessaladillo/pedrojoya/tipcalculator/ui/main/MainActivity.kt
+++ app/src/main/java/es/iessaladillo/pedrojoya/tipcalculator/ui/main/MainActivity.kt
@@ -1,9 +1,111 @@
 package es.iessaladillo.pedrojoya.tipcalculator.ui.main
 
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import es.iessaladillo.pedrojoya.tipcalculator.R
+import es.iessaladillo.pedrojoya.tipcalculator.model.TipCalculator
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    // TODO
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setupViews()
+    }
+
+    private fun setupViews() {
+        txtBill.addTextChangedListener(object: TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                checkEmptyField(txtBill)
+                val calculator = TipCalculator(
+                    txtBill.text.toString().toFloat(),
+                    txtPercentage.text.toString().toFloat(),
+                    txtDiners.text.toString().toInt()
+                )
+                setFields(calculator)
+            }
+
+        })
+        txtPercentage.addTextChangedListener(object: TextWatcher{
+            override fun afterTextChanged(p0: Editable?) {
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                checkEmptyField(txtPercentage)
+                val calculator = TipCalculator(
+                    txtBill.text.toString().toFloat(),
+                    txtPercentage.text.toString().toFloat(),
+                    txtDiners.text.toString().toInt()
+                )
+                setFields(calculator)
+            }
+
+        })
+
+        txtDiners.addTextChangedListener(object: TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                checkEmptyField(txtDiners)
+                val calculator = TipCalculator(
+                    txtBill.text.toString().toFloat(),
+                    txtPercentage.text.toString().toFloat(),
+                    txtDiners.text.toString().toInt()
+                )
+                setFields(calculator)
+            }
+
+        })
+
+        btnResetTip.setOnClickListener{ resetTip() }
+        btnResetDiners.setOnClickListener { resetDiners() }
+
+    }
+
+       private fun checkEmptyField(editText: EditText) {
+
+            if (editText.text.isEmpty()) {
+                if (editText == txtDiners)
+                    editText.setText(R.string.defaultDinersValue)
+                else if (editText == txtPercentage)
+                    editText.setText(R.string.defaultPercent)
+                else
+                    editText.setText(R.string.defaultFieldsValue)
+            }
+
+        }
+
+        private fun setFields(calculator: TipCalculator) {
+            txtTip.setText(String.format("%.2f", calculator.calculateTip()))
+            txtTotal.setText(String.format("%.2f", calculator.calculateTotal()))
+            txtPerDiner.setText(String.format("%.2f", calculator.calculatePerDiner()))
+            txtPerDinerRounded.setText(String.format("%.2f", calculator.calculatePerDinerRounded()))
+        }
+
+        private fun resetTip() {
+            txtBill.setText(R.string.defaultFieldsValue)
+            txtPercentage.setText(R.string.defaultPercent)
+        }
+
+        private fun resetDiners() {
+            txtDiners.setText(R.string.defaultDinersValue)
+        }
 }
